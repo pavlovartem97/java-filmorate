b843baba88e23ffb6f2de6d9b2378182c5b47db9
Artem, Pavlov <zlousmailpavlov@yandex.ru>
2023-02-17 00:53:20 +0300

    Add logging

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -22,6 +22,7 @@ public class FilmController {
 
     @GetMapping
     public List<Film> getAllFilms() {
+        log.info("Возвращен список фильмов " + films.toString());
         return new ArrayList<>(films.values());
     }
 
@@ -30,6 +31,7 @@ public class FilmController {
         validate(film);
         film.setId(++uresId);
         films.put(film.getId(), film);
+        log.info("Создан новый фильм: " + film);
         return film;
     }
 
@@ -37,30 +39,39 @@ public class FilmController {
     public Film update(@RequestBody Film film) throws ValidationException {
         validate(film);
         if (!films.containsKey(film.getId())) {
+            log.error("Не найдено Id фильма " + film.getId());
             throw new ValidationException("Не найдено Id фильма " + film.getId());
         }
         films.put(film.getId(), film);
+        log.info("Фильм успешно обновлен: " + film);
         return film;
     }
 
     private void validate(Film film) throws ValidationException {
         if (film.getName() == null || film.getName().isEmpty()) {
+            log.error("Название фильма не может быть пустым");
             throw new ValidationException("Название фильма не может быть пустым");
         }
         if (film.getDescription() == null) {
+            log.error("Описание фильма не может быть не задано");
             throw new ValidationException("Описание фильма не может быть не задано");
         }
         if (film.getDescription().length() > 200) {
+            log.error("Максимальная длина описания — 200 символов");
             throw new ValidationException("Максимальная длина описания — 200 символов");
         }
         if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            log.error("Дата релиза — не раньше 28 декабря 1895 года");
             throw new ValidationException("Дата релиза — не раньше 28 декабря 1895 года");
         }
         if (film.getDuration() == null) {
+            log.error("Длительность фильма не может быть не задана");
             throw new ValidationException("Длительность фильма не может быть не задана");
         }
         if (film.getDuration() < 0) {
+            log.error("Длительность фильма не может быть не задана");
             throw new ValidationException("Длительность фильма не может быть не задана");
         }
+        log.info("Валидация успешно пройдена");
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -21,6 +21,7 @@ public class UserController {
 
     @GetMapping
     public List<User> getAllFilms() {
+        log.info("Возвращен список пользователей: " + users.toString());
         return new ArrayList<>(users.values());
     }
 
@@ -29,6 +30,7 @@ public class UserController {
         validate(user);
         user.setId(++userId);
         users.put(user.getId(), user);
+        log.info("Добавлен пользователь: " + user);
         return user;
     }
 
@@ -36,36 +38,47 @@ public class UserController {
     public User update(@RequestBody User user) throws ValidationException {
         validate(user);
         if (user.getId() == null) {
+            log.error("Не задан Id пользователя");
             throw new ValidationException("Не задан Id пользователя");
         }
         if (!users.containsKey(user.getId())) {
+            log.error("Не найдено пользователя с Id " + user.getId());
             throw new ValidationException("Не найдено пользователя с Id " + user.getId());
         }
         users.put(user.getId(), user);
+        log.info("Обновлены данные пользователя: " + user);
         return user;
     }
 
     private void validate(User user) throws ValidationException {
         if (user.getLogin() == null || user.getLogin().isEmpty()) {
+            log.error("Логин не может быть пустым");
             throw new ValidationException("Логин не может быть пустым");
         }
         if (user.getLogin().contains(" ")) {
+            log.error("Логин не может содержать пробелы");
             throw new ValidationException("Логин не может содержать пробелы");
         }
-        if (user.getEmail() == null || user.getLogin().isEmpty()) {
+        if (user.getEmail() == null) {
+            log.error("Электронная почта не может быть пустой");
             throw new ValidationException("Электронная почта не может быть пустой");
         }
         if (!user.getEmail().contains("@")) {
+            log.error("Электронная почта должна содержать @");
             throw new ValidationException("Электронная почта должна содержать @");
         }
         if (user.getBirthday() == null) {
+            log.error("Дата рождения должна быть задана");
             throw new ValidationException("Дата рождения должна быть задана");
         }
         if (user.getBirthday().isAfter(LocalDate.now())) {
+            log.error("Дата рождения не может быть в будущем");
             throw new ValidationException("Дата рождения не может быть в будущем");
         }
         if (user.getName() == null || user.getName().isEmpty()) {
             user.setName(user.getLogin());
+            log.info("Пусоое имя было заменено на логин: " + user.getLogin());
         }
+        log.info("Валидация успешно пройдена");
     }
 }
