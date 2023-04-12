17c0f3322aec2fd5a3e01843b1f92e5962c4170b
Pavel Egorov <sleepwalkerpasha@ya.ru>
2023-04-12 12:14:32 +0300

    test commit

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -2,12 +2,14 @@ package ru.yandex.practicum.filmorate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.yandex.practicum.filmorate.controller.GenreController;
 
 @SpringBootApplication
 public class FilmorateApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FilmorateApplication.class, args);
+        var ctx = SpringApplication.run(FilmorateApplication.class, args);
+        System.out.println(ctx.getBean(GenreController.class));
     }
 
 }
