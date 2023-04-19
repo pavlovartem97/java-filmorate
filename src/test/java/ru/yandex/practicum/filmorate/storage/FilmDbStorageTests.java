c51b3c03163419cdde066a14612fc747aaca06d5
pavlovartem97 <111424160+pavlovartem97@users.noreply.github.com>
2023-04-19 11:39:48 +0300

    code style fixes

--- src/test/java/ru/yandex/practicum/filmorate/storage/FilmDbStorageTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/FilmDbStorageTests.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
