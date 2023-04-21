bd996bab0d022c78fc142ac14d4da881b69514b8
Pavel Egorov <sleepwalkerpasha@ya.ru>
2023-04-21 15:38:36 +0300

    optimize imports

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -1,6 +1,11 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.*;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 import javax.validation.constraints.NotBlank;
